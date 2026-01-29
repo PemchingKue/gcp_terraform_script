8266627117d2d9f5e6f6189b6ecee55fd343b37f
PemchingKue <mastervipergts@gmail.com>
2026-01-29 09:58:42 -0500

    Create main.tf
    initial commit

---
+++ quotas/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm" {
+  count        = 8
+  name         = "small-vm-${count.index + 1}"
+  machine_type = "e2-small"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 10
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+
+  tags = ["small-vm"]
+}
